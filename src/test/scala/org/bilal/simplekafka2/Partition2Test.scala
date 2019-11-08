1e4138cc1c81bbee055c2ba2c47a4fefdd9d7864
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-08 12:32:33 +0530

    added reading functionalities in sequencer file and partition

---
+++ src/test/scala/org/bilal/simplekafka2/Partition2Test.scala
@@ -0,0 +1,35 @@
+package org.bilal.simplekafka2
+
+import org.bilal.json.Codecs
+import org.bilal.simplekafka2.Partition2.Record
+import org.dist.queue.common.TopicAndPartition
+import org.dist.queue.server.Config
+import org.dist.queue.{TestUtils, ZookeeperTestHarness}
+import org.scalatest.FunSuite
+
+class Partition2Test extends FunSuite with ZookeeperTestHarness with Codecs {
+
+  test("testRead") {}
+
+  test("testAppendMessage") {
+    val partition = new Partition2(
+      Config(
+        100,
+        "localhost",
+        8000,
+        zkConnect,
+        List(TestUtils.tempDir().getAbsolutePath)
+      ),
+      TopicAndPartition("topic1", 2)
+    )
+
+    val offset1 = partition.appendMessage("key1", "hello")
+    println(offset1)
+    val offset2 = partition.appendMessage("key2", "world")
+    println(offset2)
+    val records = partition.read[String](0L)
+    val records2 = partition.read[String](0L)
+    println(records,records2)
+  }
+
+}
